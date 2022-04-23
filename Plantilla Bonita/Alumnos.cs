35add9ac3636a5c41d8849dcd860fef2bfcc7127
Víctor Hugo Carreón Pulido <hv_carreon@hotmail.com>
2022-04-22 19:41:52 -0500

    .

--- Plantilla Bonita/Alumnos.cs
+++ Plantilla Bonita/Alumnos.cs
@@ -21,5 +21,10 @@ namespace Plantilla_Bonita
         {
             this.Close();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Quibo");
+        }
     }
 }
