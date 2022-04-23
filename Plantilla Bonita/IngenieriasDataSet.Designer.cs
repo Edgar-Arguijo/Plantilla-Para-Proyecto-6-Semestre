3b721a6064e77b9536167c6534bb3bb9c82bc4f8
Víctor Hugo Carreón Pulido <hv_carreon@hotmail.com>
2022-04-23 00:46:28 -0500

    .

--- Plantilla Bonita/IngenieriasDataSet.Designer.cs
+++ Plantilla Bonita/IngenieriasDataSet.Designer.cs
@@ -730,7 +730,7 @@ namespace Plantilla_Bonita.IngenieriasDataSetTableAdapters {
         [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Data.Design.TypedDataSetGenerator", "17.0.0.0")]
         private void InitConnection() {
             this._connection = new global::System.Data.SqlClient.SqlConnection();
-            this._connection.ConnectionString = "Data Source=DESKTOP-J06FE2O;Initial Catalog=Asistencias;Integrated Security=True";
+            this._connection.ConnectionString = global::Plantilla_Bonita.Properties.Settings.Default.AsistenciasConnectionString;
         }
         
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
