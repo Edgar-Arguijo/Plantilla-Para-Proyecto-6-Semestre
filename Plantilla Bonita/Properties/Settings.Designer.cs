833be0d1a085c9443330f7e6e3ac736a7ba94d56
arguijo vazquez <arguijovazquez@hotmail.com>
2022-04-24 14:20:34 -0500

    .

--- Plantilla Bonita/Properties/Settings.Designer.cs
+++ Plantilla Bonita/Properties/Settings.Designer.cs
@@ -26,7 +26,6 @@ namespace Plantilla_Bonita.Properties {
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
-        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=DESKTOP-2HNOC1U;Initial Catalog=Asistencias;Integrated Security=True")]
         public string CadenaDeConexionAplicacion {
             get {
                 return ((string)(this["CadenaDeConexionAplicacion"]));
