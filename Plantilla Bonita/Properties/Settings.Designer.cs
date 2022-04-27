8f043edb6438f4ca66943c8245824b8124236769
arguijo vazquez <arguijovazquez@hotmail.com>
2022-04-26 22:51:03 -0500

    .

--- Plantilla Bonita/Properties/Settings.Designer.cs
+++ Plantilla Bonita/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace Plantilla_Bonita.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.1.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.10.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -26,8 +26,7 @@ namespace Plantilla_Bonita.Properties {
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
-        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=DESKTOP-J06FE2O1;Initial Catalog=Asistencias;Integrated Security=True" +
-            "")]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=DESKTOP-2HNOC1U;Initial Catalog=Asistencias;Integrated Security=True")]
         public string CadenaDeConexionAplicacion {
             get {
                 return ((string)(this["CadenaDeConexionAplicacion"]));
