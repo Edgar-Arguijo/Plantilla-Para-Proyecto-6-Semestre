38bee8ec2b0cbd82932eaa7459c697898009eee5
Víctor Hugo Carreón Pulido <hv_carreon@hotmail.com>
2022-06-04 14:05:45 -0500

    Se movio la clase de encriptado a Acceso_A_Datos

--- Acceso_A_Datos/Consultas.cs
+++ Acceso_A_Datos/Consultas.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -11,6 +12,46 @@ namespace Acceso_A_Datos
 {
     public class Consultas : SQLConection
     {
+        internal class Encriptado_Desencriptado
+        {
+            private static string hash = "clave:0";
+
+            public static string Encriptar(string cadena)
+            {
+
+                byte[] data = UTF8Encoding.UTF8.GetBytes(cadena);
+                using (MD5CryptoServiceProvider md5 = new MD5CryptoServiceProvider())
+                {
+                    byte[] keys = md5.ComputeHash(UTF8Encoding.UTF8.GetBytes(hash));
+                    using (TripleDESCryptoServiceProvider tripOes = new TripleDESCryptoServiceProvider()
+                    { Key = keys, Mode = CipherMode.ECB, Padding = PaddingMode.PKCS7 })
+                    {
+                        ICryptoTransform transform = tripOes.CreateEncryptor();
+                        byte[] result = transform.TransformFinalBlock(data, 0, data.Length);
+                        return Convert.ToBase64String(result, 0, result.Length);
+                    }
+                }
+            }
+
+            public static string Desencriptar(string cadena)
+            {
+                byte[] data = Convert.FromBase64String(cadena);
+
+
+                using (MD5CryptoServiceProvider md5 = new MD5CryptoServiceProvider())
+                {
+                    byte[] keys = md5.ComputeHash(UTF8Encoding.UTF8.GetBytes(hash));
+                    using (TripleDESCryptoServiceProvider tripOes = new TripleDESCryptoServiceProvider()
+                    { Key = keys, Mode = CipherMode.ECB, Padding = PaddingMode.PKCS7 })
+                    {
+                        ICryptoTransform transform = tripOes.CreateDecryptor();
+                        byte[] result = transform.TransformFinalBlock(data, 0, data.Length);
+                        return UTF8Encoding.UTF8.GetString(result, 0, result.Length);
+                    }
+                }
+            }
+        }
+
         public void ProbarConexion() {
             using (SqlConnection conexion = getConnection())
             {
@@ -54,7 +95,7 @@ namespace Acceso_A_Datos
                     {
                         cmd.CommandType = CommandType.StoredProcedure;
                         cmd.Parameters.AddWithValue("@usuario", usuario);
-                        cmd.Parameters.AddWithValue("@contraseña", contra);
+                        cmd.Parameters.AddWithValue("@contraseña", Encriptado_Desencriptado.Encriptar(contra));
                         cmd.Parameters.AddWithValue("@fecha", DateTime.Now);
                         return (cmd.ExecuteNonQuery()>0);
                     }
@@ -108,10 +149,9 @@ namespace Acceso_A_Datos
                 {
                     try
                     {
-
                         cmd.CommandType = CommandType.StoredProcedure;
                         cmd.Parameters.AddWithValue("@usuario", usuario);
-                        cmd.Parameters.AddWithValue("@contraseña", contraseña);
+                        cmd.Parameters.AddWithValue("@contraseña", Encriptado_Desencriptado.Encriptar(contraseña));
                         cmd.Parameters.AddWithValue("@fecha", DateTime.Now);
                         cmd.Parameters.Add("@level", SqlDbType.VarChar, Int32.MaxValue).Direction = ParameterDirection.Output;
                         cmd.ExecuteNonQuery();
