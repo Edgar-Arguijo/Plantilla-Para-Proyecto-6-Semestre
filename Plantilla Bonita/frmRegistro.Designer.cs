35755fe54441bdea8d3e5a3e1a88dec2092c2075
Víctor Hugo Carreón Pulido <hv_carreon@hotmail.com>
2022-06-04 14:08:04 -0500

    .

--- Plantilla Bonita/frmRegistro.Designer.cs
+++ Plantilla Bonita/frmRegistro.Designer.cs
@@ -30,88 +30,127 @@
         {
             this.txtNomUsuario = new System.Windows.Forms.TextBox();
             this.txtContra = new System.Windows.Forms.TextBox();
-            this.btnRegistro = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.txtConfContra = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnRegistrarse = new System.Windows.Forms.Button();
+            this.pblEncabezado = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // txtNomUsuario
             // 
-            this.txtNomUsuario.Location = new System.Drawing.Point(41, 100);
+            this.txtNomUsuario.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.txtNomUsuario.Location = new System.Drawing.Point(39, 91);
             this.txtNomUsuario.Name = "txtNomUsuario";
-            this.txtNomUsuario.Size = new System.Drawing.Size(212, 20);
+            this.txtNomUsuario.Size = new System.Drawing.Size(212, 26);
             this.txtNomUsuario.TabIndex = 0;
             // 
             // txtContra
             // 
-            this.txtContra.Location = new System.Drawing.Point(41, 162);
+            this.txtContra.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.txtContra.Location = new System.Drawing.Point(39, 166);
             this.txtContra.Name = "txtContra";
-            this.txtContra.Size = new System.Drawing.Size(212, 20);
+            this.txtContra.Size = new System.Drawing.Size(212, 26);
             this.txtContra.TabIndex = 1;
-            // 
-            // btnRegistro
-            // 
-            this.btnRegistro.Location = new System.Drawing.Point(66, 310);
-            this.btnRegistro.Name = "btnRegistro";
-            this.btnRegistro.Size = new System.Drawing.Size(145, 23);
-            this.btnRegistro.TabIndex = 2;
-            this.btnRegistro.Text = "Registrarse";
-            this.btnRegistro.UseVisualStyleBackColor = true;
-            this.btnRegistro.Click += new System.EventHandler(this.btnRegistro_Click);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(38, 78);
+            this.label1.BackColor = System.Drawing.Color.LemonChiffon;
+            this.label1.Font = new System.Drawing.Font("Bahnschrift Condensed", 15.75F);
+            this.label1.Location = new System.Drawing.Point(34, 54);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(98, 13);
+            this.label1.Size = new System.Drawing.Size(143, 25);
             this.label1.TabIndex = 3;
             this.label1.Text = "Nombre de Usuario";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(38, 137);
+            this.label2.BackColor = System.Drawing.Color.LemonChiffon;
+            this.label2.Font = new System.Drawing.Font("Bahnschrift Condensed", 15.75F);
+            this.label2.Location = new System.Drawing.Point(34, 129);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(61, 13);
+            this.label2.Size = new System.Drawing.Size(94, 25);
             this.label2.TabIndex = 4;
             this.label2.Text = "Contraseña";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(43, 203);
+            this.label3.BackColor = System.Drawing.Color.LemonChiffon;
+            this.label3.Font = new System.Drawing.Font("Bahnschrift Condensed", 15.75F);
+            this.label3.Location = new System.Drawing.Point(34, 204);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(118, 13);
+            this.label3.Size = new System.Drawing.Size(181, 25);
             this.label3.TabIndex = 6;
             this.label3.Text = "Confirme su contraseña";
             // 
             // txtConfContra
             // 
-            this.txtConfContra.Location = new System.Drawing.Point(46, 228);
+            this.txtConfContra.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.txtConfContra.Location = new System.Drawing.Point(39, 241);
             this.txtConfContra.Name = "txtConfContra";
-            this.txtConfContra.Size = new System.Drawing.Size(212, 20);
+            this.txtConfContra.Size = new System.Drawing.Size(212, 26);
             this.txtConfContra.TabIndex = 5;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.LemonChiffon;
+            this.panel1.Controls.Add(this.pblEncabezado);
+            this.panel1.Controls.Add(this.btnRegistrarse);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.txtNomUsuario);
+            this.panel1.Controls.Add(this.txtConfContra);
+            this.panel1.Controls.Add(this.txtContra);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(291, 369);
+            this.panel1.TabIndex = 7;
+            // 
+            // btnRegistrarse
+            // 
+            this.btnRegistrarse.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(1)))));
+            this.btnRegistrarse.FlatAppearance.BorderSize = 0;
+            this.btnRegistrarse.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnRegistrarse.Font = new System.Drawing.Font("Bahnschrift Condensed", 15.75F);
+            this.btnRegistrarse.ForeColor = System.Drawing.Color.White;
+            this.btnRegistrarse.Location = new System.Drawing.Point(61, 301);
+            this.btnRegistrarse.Name = "btnRegistrarse";
+            this.btnRegistrarse.Size = new System.Drawing.Size(167, 45);
+            this.btnRegistrarse.TabIndex = 7;
+            this.btnRegistrarse.Text = "Registrarse";
+            this.btnRegistrarse.UseVisualStyleBackColor = false;
+            this.btnRegistrarse.Click += new System.EventHandler(this.btnRegistrarse_Click);
+            // 
+            // pblEncabezado
+            // 
+            this.pblEncabezado.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(1)))));
+            this.pblEncabezado.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pblEncabezado.Location = new System.Drawing.Point(0, 0);
+            this.pblEncabezado.Name = "pblEncabezado";
+            this.pblEncabezado.Size = new System.Drawing.Size(291, 38);
+            this.pblEncabezado.TabIndex = 8;
             // 
             // frmRegistro
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(301, 450);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.txtConfContra);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.btnRegistro);
-            this.Controls.Add(this.txtContra);
-            this.Controls.Add(this.txtNomUsuario);
+            this.ClientSize = new System.Drawing.Size(291, 369);
+            this.Controls.Add(this.panel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "frmRegistro";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
             this.Text = "frmRegistro";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -119,10 +158,12 @@
 
         private System.Windows.Forms.TextBox txtNomUsuario;
         private System.Windows.Forms.TextBox txtContra;
-        private System.Windows.Forms.Button btnRegistro;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.TextBox txtConfContra;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnRegistrarse;
+        private System.Windows.Forms.Panel pblEncabezado;
     }
 }
