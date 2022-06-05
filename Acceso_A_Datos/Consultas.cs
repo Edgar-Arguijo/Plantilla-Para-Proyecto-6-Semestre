3892658ac54e80de8242b203e83285c94107690a
Víctor Hugo Carreón Pulido <hv_carreon@hotmail.com>
2022-06-05 15:43:40 -0500

    Se agrego un mensaje a consola

--- Acceso_A_Datos/Consultas.cs
+++ Acceso_A_Datos/Consultas.cs
@@ -64,6 +64,7 @@ namespace Acceso_A_Datos
             }
             catch (System.Data.SqlClient.SqlException ex)
             {
+                Console.WriteLine(ex.Message);
                 MessageBox.Show("Porfavor consulte al administrador", "Error con la base de datos", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 Environment.Exit(0);
             }
@@ -116,6 +117,40 @@ namespace Acceso_A_Datos
                         return false;
                     }
                 }
+            }
+        }
+
+        public bool AltaIngenieria(string usuario, string cod, string desc) 
+        {
+            try
+            {
+                using (SqlConnection conexion = getConnection())
+                {
+                    conexion.Open();
+                    using (SqlCommand cmd = new SqlCommand("AltaIngenieria", conexion)) 
+                    {
+                        cmd.CommandType = CommandType.StoredProcedure;
+                        cmd.Parameters.AddWithValue("@usuario@codIngenieria", usuario);
+                        cmd.Parameters.AddWithValue("@codIngenieria", cod);
+                        cmd.Parameters.AddWithValue("@Nombre", desc);
+                        cmd.Parameters.AddWithValue("@Fecha", DateTime.Now);
+                        cmd.Parameters.Add("@flag", SqlDbType.Bit).Direction = ParameterDirection.ReturnValue;
+                        cmd.ExecuteNonQuery();
+
+                        if (cmd.Parameters["@flag"].Value.ToString() == "0")
+                            return false;
+
+                        if (cmd.Parameters["@flag"].Value.ToString() == "1")
+                            return true;
+
+                        return false;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return false;
             }
         }
 
