8c78008f31af2af08bebe8f591abe5a044fc2c96
Andrea Evelyn Mejía Rubio <69946882+mejiaandiem3@users.noreply.github.com>
2022-06-13 14:57:04 -0500

    .

--- Plantilla Bonita/Properties/Settings.Designer.cs
+++ Plantilla Bonita/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace SGA_ITSL.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.1.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.2.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -27,7 +27,7 @@ namespace SGA_ITSL.Properties {
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
         [global::System.Configuration.DefaultSettingValueAttribute("Data Source=sgaitsldb.database.windows.net;Initial Catalog=Asistencia;Persist Sec" +
-            "urity Info=True;User ID=victor;Password=4KvpUD")]
+            "urity Info=True;User ID=victor;Password=4KvpUD<4(?4")]
         public string AsistenciaConnectionString {
             get {
                 return ((string)(this["AsistenciaConnectionString"]));
